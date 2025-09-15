2b9d84f3ab9a7dfb3c820ff9541ca63ce9998b5a
AjithaRQ <ajitha.devi@rootquotient.com>
2025-09-15 15:28:26 +0530

    errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
+  var a = "test";
   return twMerge(clsx(inputs));
 }
 
